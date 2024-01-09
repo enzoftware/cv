bf77ad3ac608a0d80c525c8ddbcc112d9ba9cbfe
Enzo Lizama <lizama.enzo@gmail.com>
2024-01-09 02:09:56 -0500

    chore: update information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,6 +88,14 @@ export default function Page() {
           </p>
         </Section>
         <Section>
+          <h2 className="text-xl font-bold">Skills</h2>
+          <div className="flex flex-wrap gap-1">
+            {RESUME_DATA.skills.map((skill) => {
+              return <Badge key={skill}>{skill}</Badge>;
+            })}
+          </div>
+        </Section>
+        <Section>
           <h2 className="text-xl font-bold">Work Experience</h2>
           {RESUME_DATA.work.map((work) => {
             return (
@@ -146,14 +154,6 @@ export default function Page() {
               </Card>
             );
           })}
-        </Section>
-        <Section>
-          <h2 className="text-xl font-bold">Skills</h2>
-          <div className="flex flex-wrap gap-1">
-            {RESUME_DATA.skills.map((skill) => {
-              return <Badge key={skill}>{skill}</Badge>;
-            })}
-          </div>
         </Section>
 
         <Section className="print-force-new-page scroll-mb-16">
